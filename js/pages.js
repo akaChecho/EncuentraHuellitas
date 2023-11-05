af6b3a27ee49b678c78ac33a6f07f3669dac304d
walter <walter.ezequiel.reyes@gmail.com>
2023-11-05 05:00:54 -0300

    fix excepcion js v.1.004

--- js/pages.js
+++ js/pages.js
@@ -1,21 +1,41 @@
-(async () => {
-    // Obtener el contenido del archivo HTML
+// (async () => {
+//     // Obtener el contenido del archivo HTML
+//     const response = await fetch("./Resources/pages/navbar.html");
+//     const html = await response.text();
+  
+//     // Insertar el contenido en el DOM
+//     document.querySelector("#navbar").innerHTML = html;
+
+//   // Escuchar el evento popstate
+//   })();
+
+// // Cargar el navbar inicialmente
+// (async () => {
+//     await loadNavbar();
+//   })();
+  
+//   // Escuchar el evento popstate y recargar el navbar
+//   window.addEventListener("popstate", async () => {
+//     // Cargar el navbar
+//     await loadNavbar();
+//   });
+// Define the loadNavbar function
+async function loadNavbar() {
+    // Get the navbar content
     const response = await fetch("./Resources/pages/navbar.html");
     const html = await response.text();
   
-    // Insertar el contenido en el DOM
+    // Insert the content into the DOM
     document.querySelector("#navbar").innerHTML = html;
-
-  // Escuchar el evento popstate
-  })();
-
-// Cargar el navbar inicialmente
-(async () => {
-    await loadNavbar();
-  })();
+  }
   
-  // Escuchar el evento popstate y recargar el navbar
-  window.addEventListener("popstate", async () => {
-    // Cargar el navbar
-    await loadNavbar();
-  });
+  // Cargar el navbar inicialmente
+  (async () => {
+      await loadNavbar();
+    })();
+    
+    // Escuchar el evento popstate y recargar el navbar
+    window.addEventListener("popstate", async () => {
+      // Cargar el navbar
+      await loadNavbar();
+    });
