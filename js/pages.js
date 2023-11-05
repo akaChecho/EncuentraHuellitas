53b64c1ea2f0f4f3c80823dac4299e867b5ab8e8
walter <walter.ezequiel.reyes@gmail.com>
2023-11-05 05:28:29 -0300

    v.1.005

--- js/pages.js
+++ js/pages.js
@@ -1,24 +1,3 @@
-// (async () => {
-//     // Obtener el contenido del archivo HTML
-//     const response = await fetch("./Resources/pages/navbar.html");
-//     const html = await response.text();
-  
-//     // Insertar el contenido en el DOM
-//     document.querySelector("#navbar").innerHTML = html;
-
-//   // Escuchar el evento popstate
-//   })();
-
-// // Cargar el navbar inicialmente
-// (async () => {
-//     await loadNavbar();
-//   })();
-  
-//   // Escuchar el evento popstate y recargar el navbar
-//   window.addEventListener("popstate", async () => {
-//     // Cargar el navbar
-//     await loadNavbar();
-//   });
 // Define the loadNavbar function
 async function loadNavbar() {
     // Get the navbar content
